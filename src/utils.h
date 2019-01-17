5d4353838fb755d4bf95cd4d3d8db0622d26e21d
Konstantinos Kamaropoulos <kamaropoulos@outlook.com>
2019-01-17 02:18:11 +0200

    Added Doxygen HTML Docs

--- src/utils.h
+++
@@ -1,5 +0,0 @@
-#define execCallbackIf(statement, callback) if (statement) callback
-
-#define ifMyRankEquals(myRank, rank, callback) executeCallbackIf(myRank == rank, callback);
-
-#define ifRoot(callback) if (myRank == ROOT_PROCESS) callback
